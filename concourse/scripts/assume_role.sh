0611b65cc8f5915a09b4ac08055e1518e840c932
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-07-14 12:49:19 +0100

    remove shellcheck space

--- concourse/scripts/assume_role.sh
+++ concourse/scripts/assume_role.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-# shellcheck disable=SC3040, SC2154
+# shellcheck disable=SC3040,SC2154
 aws sts assume-role --output text \
     --role-arn "${aws_role_arn}" \
     --role-session-name concourse-pipeline-run \
     --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
     | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
-# shellcheck disable=SC3040, SC2154
+# shellcheck disable=SC3040,SC2154
 
 
 AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
