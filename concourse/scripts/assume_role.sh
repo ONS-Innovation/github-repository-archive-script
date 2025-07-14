6b7355f01412df9447c0ea04cc3443e3a57c5844
Hasan <31344273+delterr@users.noreply.github.com>
2025-07-14 17:29:53 +0100

    Concourse pipeline (#8)
    * init concourse
    
    * put quotes around variable declaration
    
    * remove concourse from branch check
    
    * use correct repo name
    
    * use correct directory
    
    * remove stray cd
    
    * refactor to use API for tagging
    
    * github_repo_archive_secrets -> repo_archive secrets
    
    * bump provider version to 6.2.0
    
    * remove concourse conditional
    
    * update README
    
    * lint bash scripts
    
    * lint some more
    
    * remove shellcheck space
    
    * run prettier
    
    * Revert "run prettier"
    
    This reverts commit 873b10e7c3596a0a3eebc23d5d1f7e56f27d5326.
    
    * add whitespace
    
    * remove shebang line
    
    * add sh-shebang and lint yaml
    
    * disable SC3040
    
    * fix yaml file and add in SC2148
    
    * fix yaml
    
    * remove container image from terraform_infra.sh

---
+++ concourse/scripts/assume_role.sh
@@ -0,0 +1,19 @@
+# shellcheck disable=SC3040,SC2154,SC2148
+
+set -euo pipefail
+
+# shellcheck disable=SC3040,SC2154
+aws sts assume-role --output text \
+    --role-arn "${aws_role_arn}" \
+    --role-session-name concourse-pipeline-run \
+    --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+    | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
+
+# shellcheck disable=SC3040,SC2154
+AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
+AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
+AWS_SESSION_TOKEN="$(cat SessionToken)"
+
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
---
+++ concourse/scripts/set_pipeline.sh
@@ -0,0 +1,29 @@
+# shellcheck disable=SC3040,SC2154,SC2148
+
+repo_name=${1}
+
+if [[ $# -gt 1 ]]; then
+    branch=${2}
+    git rev-parse --verify "${branch}"
+    # shellcheck disable=SC2181
+    if [[ $? -ne 0 ]]; then
+        echo "Branch \"${branch}\" does not exist"
+        exit 1
+    fi
+else
+    branch=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+if [[ ${branch} == "main" || ${branch} == "master" ]]; then
+    env="prod"
+else
+    env="dev"
+fi
+
+if [[ ${env} == "dev" ]]; then
+    tag=$(git rev-parse HEAD)
+else
+    tag=$(git tag | tail -n 1)
+fi
+
+fly -t aws-sdp set-pipeline -c concourse/ci.yml -p "${repo_name}"-"${branch}" -v branch="${branch}" -v tag="${tag}" -v env="${env}"
