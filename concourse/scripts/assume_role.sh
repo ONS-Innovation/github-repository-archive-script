0a1cd329ce4600ff5ac99be94da2268edcb67103
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-07-14 15:40:03 +0100

    add whitespace

--- concourse/scripts/assume_role.sh
+++ concourse/scripts/assume_role.sh
@@ -8,9 +8,8 @@ aws sts assume-role --output text \
     --role-session-name concourse-pipeline-run \
     --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
     | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
+
 # shellcheck disable=SC3040,SC2154
-
-
 AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
 AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
 AWS_SESSION_TOKEN="$(cat SessionToken)"
