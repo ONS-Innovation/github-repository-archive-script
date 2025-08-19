5a9ffe1c1ab6531ded420b311a31243dbd459459
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-08-19 15:34:47 +0100

    fix(terraform linting): Pin to a git release instead of a tf reg release.

--- terraform/service/eventbridge.tf
+++ terraform/service/eventbridge.tf
@@ -1,6 +1,9 @@
 module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
-  version = "4.1.0"
+  # source  = "terraform-aws-modules/eventbridge/aws"
+  # version = "4.1.0"
+
+  # Pin to a git commit instead. This is the same as the above.
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-eventbridge.git?ref=3e8657cd925d5b4a21301a09b67d8081f24bcfc3"
 
   role_name = "${var.lambda_name}-eventbridge-role"
 
